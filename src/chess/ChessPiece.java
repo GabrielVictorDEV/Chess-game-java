4976c2b0b51e1432f6d48183a877cb57e4034ec2
gabriel-victor-home <gv2074300@gmail.com>
2023-01-27 14:29:22 -0300

    count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,7 +8,8 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece{
 	
 	private Color color; //cor da peça; 
-
+	private int moveCount; 
+	
 	public ChessPiece(Board board, Color color) { //Peça que tem um tabuleiro é uma cor; 
 		super(board);
 		this.color = color;
@@ -18,6 +19,18 @@ public abstract class ChessPiece extends Piece{
 		return color;
 	}	
 
+	public int getMoveCount() {
+		return moveCount;
+	}
+
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
